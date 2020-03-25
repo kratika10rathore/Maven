99aeba4dbe61fc6bf507f08d03fa2ceadacfc850
kratika10rathore <61554796+kratika10rathore@users.noreply.github.com>
2020-03-25 16:28:41 +0530

    Update Krats.java

--- maven-project/src/main/java/com/kratika/Krats.java
+++ maven-project/src/main/java/com/kratika/Krats.java
@@ -4,5 +4,8 @@ public class Krats {
 	public String Sample(){
 		return "Hello Kratika";
 	}
-
+        public int addNumbers(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
 }
